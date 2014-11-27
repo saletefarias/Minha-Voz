d8b9805b142c6183c05e860a6f5a22d25b50a531
Salete Farias <saletefarias@gmail.com>
2014-11-27 13:44:51 -0200

    Create meusdireitos.php

---
+++ meusdireitos.php
@@ -0,0 +1,96 @@
+<section id="meusdireitos" class="bg-light-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Meus Direitos</h2>
+                    <h3 class="section-subheading text-muted">Descrição</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="img/portfolio/lei-maria-da-penha.jpg" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>Lei Maria da Penha</h4>
+                        <p class="text-muted">DESCRIÇÃO LEI</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                       <img src="img/portfolio/lei-maria-da-penha.jpg" class="img-responsive" alt="">
+                    </a>
+                     <div class="portfolio-caption">
+                        <h4>Lei Maria da Penha</h4>
+                        <p class="text-muted">DESCRIÇÃO LEI</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                       <img src="img/portfolio/lei-maria-da-penha.jpg" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                         <h4>Lei Maria da Penha</h4>
+                        <p class="text-muted">DESCRIÇÃO LEI</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal4" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                      <img src="img/portfolio/lei-maria-da-penha.jpg" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                         <h4>Lei Maria da Penha</h4>
+                        <p class="text-muted">DESCRIÇÃO LEI</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal5" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                       <img src="img/portfolio/lei-maria-da-penha.jpg" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                         <h4>Lei Maria da Penha</h4>
+                        <p class="text-muted">DESCRIÇÃO LEI</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a href="#portfolioModal6" class="portfolio-link" data-toggle="modal">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                      <img src="img/portfolio/lei-maria-da-penha.jpg" class="img-responsive" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                         <h4>Lei Maria da Penha</h4>
+                        <p class="text-muted">DESCRIÇÃO LEI</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>

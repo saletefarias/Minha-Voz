d3057f87db40f2f96bb2cd900e335b03b8a0e642
SALETE FARIAS <saletefarias@gmail.com>
2014-12-05 01:26:51 -0300

    Minha Voz versao1_2_0

--- header.php
+++ header.php
@@ -19,4 +19,4 @@
 	<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
 	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 	<script type="text/javascript" src="js/jquery.scrollTo-1.3.3-min.js"></script>
-	<script type="text/javascript" src="js/decisionTree.js"></script>
+
--- menu.php
+++ menu.php
@@ -1,19 +1,21 @@
   <!-- Navigation -->
+  
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
+            
+			<div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
+                   
+					<span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a  class="navbar-brand page-scroll" href="#page-top"><img src="img/logos/logo.png" class="logo"></a>
-            </div>
 
+				</button>
+			   <a  class="navbar-brand page-scroll" href="#page-top"><img src="img/logos/logo.png" class="logo"/>Beta</a></li></ul>
+            </div>
+		
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">	 
                 <ul class="nav navbar-nav navbar-right">
                     <li class="hidden">
                         <a href="#page-top"></a>
@@ -31,7 +33,7 @@
                         <a class="page-scroll" href="#direitodesrespeitado">Depoimentos</a>
                     </li>
 					<li> 
-                        <a class="page-scroll" href="#meusdireitos">Quem somos</a>
+                        <a class="page-scroll" href="#quemsomos">Quem somos</a>
                     </li>
                     <li>
                         <a class="page-scroll" href="#contact">Contato</a>
